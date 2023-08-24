1e6266d66493fd54582d46472d847a7d225bd4ff
Ali Hassan <hassanali8812@gmail.com>
2023-08-24 13:03:12 +0500

    Second

---
+++ script.js
@@ -0,0 +1,53 @@
+function togglePassword(id){
+    const x = document.getElementById(id)
+    if (x.type === "password") {
+        x.type = "text";
+      } else {
+        x.type = "password";
+      }
+}
+
+
+
+const xValues = [100,200,300,400,500,600,700,800,900,1000];
+
+new Chart("chart", {
+  type: "line",
+  data: {
+    labels: xValues,
+    datasets: [{
+      data: [860,1070,1110,1330,2210,7830,2478],
+      borderColor: "red",
+      fill: false
+    },{
+      data: [1600,1700,1700,1900,2000,2700,4000,5000,6000,7000],
+      borderColor: "green",
+      fill: false
+    },{
+      data: [300,700,2000,5000,6000,4000,2000,1000,200,100],
+      borderColor: "blue",
+      fill: false
+    }]
+  },
+  options: {
+    legend: {display: false},
+    scales: {
+      xAxes: [{
+          gridLines: {
+              color: "rgba(0, 0, 0, 0)",
+          }
+      }],
+      xAxes: [{
+        display: false //this will remove all the x-axis grid lines
+      }],
+      yAxes: [{
+        gridLines: {
+          drawBorder: false
+        },
+        ticks: {
+          maxTicksLimit: 5
+        }
+      }]
+  }
+  }
+});
